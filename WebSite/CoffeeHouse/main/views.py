3edb025d07b2f12c6b283314049534c87be9ff0a
Gorilla73 <anton.gavrilov.1673@mail.ru>
2021-12-24 15:39:34 +0300

    начал работу с корзиной, но пока возникают ошибки, которые нужно когда нибудт исправить

--- WebSite/CoffeeHouse/main/views.py
+++ WebSite/CoffeeHouse/main/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView, View
 
-from .models import HotDrinks, ColdDrinks, Desserts, Category, LatestProducts
-from .mixins import CategoryDetailMixin
+from .models import HotDrinks, ColdDrinks, Desserts, Category, LatestProducts, Customer, Cart, CartProduct
+from .mixins import CategoryDetailMixin, CartMixin
 
 
-class BaseView(View):
+class BaseView(CartMixin, View):
     def get(self, request, *args, **kwargs):
+
         categories = Category.objects.get_categories_for_cap()
         products = LatestProducts.objects.get_product_for_main_page('hotdrinks', 'colddrinks', 'desserts')
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': self.cart
         }
         return render(request, 'base.html', context)
 
 
-class ProductDetailView(CategoryDetailMixin, DetailView):
+class ProductDetailView(CartMixin, CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'hotdrinks': HotDrinks,
@@ -33,11 +37,48 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
 
-class CategoryDetailView(CategoryDetailMixin, DetailView):
+
+class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
 
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class AddToCartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.create(
+            user=self.cart.owner, cart=self.cart, content_type=content_type, object_id=product.id
+        )
+        if created:
+            self.cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
+
+
+class CartView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_cap()
+        context = {
+            'cart': self.cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
+
+
+
+
+
+
+
