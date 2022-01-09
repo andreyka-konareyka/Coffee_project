c107def7e2d703c868da7e631b66f7cfea0cfbbe
Russakura8 <vbifntht@gmail.com>
2022-01-09 13:50:56 +0300

    Подключил БД

---
+++ src/test/java/pivo_project/cofee/CofeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pivo_project.cofee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
